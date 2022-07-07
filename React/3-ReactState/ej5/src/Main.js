52e2fa40801bd84d83871d22c2b3eedc0e6e1151
Manuel Muñoz <manuel.mz.mn@gmail.com>
2022-07-08 00:25:46 +0200

    commit-3

--- React/3-ReactState/ej5/src/Main.js
+++ React/3-ReactState/ej5/src/Main.js
@@ -2,52 +2,33 @@ import { useState } from 'react'
 
 function Tarjeta(props) {
    let [stock, setStock] = useState(3)
-   let [mensaje, setMensaje] = useState("Stock")
 
-   function añadir() {
-      setStock(stock - 1)
-      setMensaje(mensaje, "Stock")
-      if (stock === 0) {
-         setStock(stock, 0)
-         setMensaje("Sin Stock")
-      }
-   }
-
-
-   let id = props.id
    return (
       <div className="container" >
          <div className="container card">
-            <h2>{props.title}</h2>
-            <img src={props.formats["image/jpeg"]} alt={props.title} />
+            <h2>{props.libro.title}</h2>
+            <img src={props.libro.formats["image/jpeg"]} alt={props.libro.title} />
             <ul>
-               <li><h4>Autor: {props.authors.map((autor) => (autor.name))}</h4></li>
-               <li>ID: {id}</li>
-               <li>Genero: {props.subjects[0]}</li>
-               <li>Descargas: {props.download_count}</li>
-               <li>Lenguajes: {props.languages}</li>
+               <li><h4>Autor: {props.libro.authors.map((autor) => (autor.name))}</h4></li>
+               <li>ID: {props.libro.id}</li>
+               <li>Genero: {props.libro.subjects[0]}</li>
+               <li>Descargas: {props.libro.download_count}</li>
+               <li>Lenguajes: {props.libro.languages}</li>
 
             </ul>
-            <p>{mensaje}: {stock}</p>
-            <button onClick={añadir}>Añadir al carrito</button>
+            <p>{stock<=0?"sin stock":"stock: " +stock}</p>
+            <button onClick={()=>setStock(stock -1)}>Añadir al carrito</button>
          </div>
       </div>
    )
 }
-
-
 function Main(props) {
-   console.log(props)
    return <main>{props.libros.map((libro, index) => {
       
       return (<Tarjeta
-         titulo={libro.title}
-         imagen={libro.formats["image/jpeg"]}
-         autor={libro.authors.map((autor) => (autor.name))}
-         id={libro.id}
-         genero={libro.subjects[index]}
-         descargas={libro.download_count}
-         lenguages={libro.languages} />
+      key ={index}
+      libro={libro}
+         />
       )
    })}</main>
 }
